f1b850afa206cd010128a88a4eecce9f07a6ac17
Jhonah <buryakone@gmail.com>
2020-06-30 13:52:08 +0700

    taks 2 of the project is completed - hexdecimal background color change

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 //const button = document.querySelector('button');
 const body = document.querySelector('body');
 const button = document.querySelector('a');
-const span = document.querySelector('span');
+//const span = document.querySelector('span');
+const span = document.querySelector('#hex-value');
 
 button.addEventListener('click', /*event*/() => {
   /*button.innerHTML = `Click count: ${event.detail}`;*/
@@ -13,7 +14,8 @@ button.addEventListener('click', /*event*/() => {
   /*
     https://stackoverflow.com/questions/1533568/what-is-the-correct-way-to-write-html-using-javascript
   */
-  span.innerHTML = color;
+  //span.innerHTML = color;
+  span.textContent = color;
   //console.log(color);
 });
 
@@ -34,6 +36,7 @@ function setRandomColor(color) {
 }
 
 /* 
+  task 1 - Implement the JavaScript code such that every time the button is clicked, the background changes color.
   ready code copied from https://romeojeremiah.github.io/project_change_color_background/
 
 #const button = document.querySelector('button')
@@ -47,4 +50,30 @@ function setRandomColor(color) {
 #const colorIndex= parseInt(Math.random()*colors.length)
 #body.style.backgroundColor = colors[colorIndex]
 #}
+*/
+
+/*
+  task 2 - Every time the button is clicked, the background should changes color and you should show the Hex value of the color.
+  ready code copied from 
+  https://romeojeremiah.github.io/hex-color-background-project/
+
+#(function() {
+#    const button = document.querySelector('#btn')
+#    const body = document.querySelector('body')
+#    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+#    const value = document.querySelector('#hex-value')
+#
+#    button.addEventListener('click', changeHex)
+#
+#    function changeHex(){
+#        let hex = '#'
+#
+#        for (let i = 0; i < 6; i++){
+#            const index = Math.floor(Math.random()*hexValues.length)
+#            hex += hexValues[index]
+#        }
+#        value.textContent = hex
+#        body.style.backgroundColor = hex
+#    }
+#} )()  
 */
